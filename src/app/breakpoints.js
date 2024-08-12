d56d7c1625695c2c89aa79daaf0caaad72c30553
jotaxv <euujoauux317@gmail.com>
2024-08-12 16:39:57 -0300

    update.1

---
+++ src/app/breakpoints.js
@@ -0,0 +1,11 @@
+const size = {
+    sm: '640px',
+    md: '800px',
+    bg: '1300px'
+}
+const breakpoints = {
+    sm: '(max-width: ${size.sm})',
+    md: '(max-width: ${size.md})',
+    bg: '(max-width: ${size.bg})'
+}
+export default breakpoints
--- src/app/page.js
+++ src/app/page.js
@@ -1,60 +1,61 @@
 import Image from "next/image";
 import style from "./page.css";
 import Input from "postcss/lib/input";
+import breakpoints from "./breakpoints";
 
 export default function Home() {
   return (
-    <main className="m-0 p-0 flex items-center justify-center bg-green-100 h-[100vh]">
-      <div className="bg-white flex justify-center w-[600px] p-[30px] rounded-[10px] flex-col">
-        <h1 className="font-[600] text-[22px] mb-[20px]">Contact Us</h1>
-        <div className="flex flex-row gap-5 m-[12px]">
+    <main id="tantoFaz" className="m-0 flex items-center justify-center bg-green-200 p-[10px] ">
+      <div className="bg-white flex justify-center w-[390px] p-[15px] rounded-[12px] flex-col lg:w-[690px]">
+        <h1 className="font-[600] text-[35px] mb-[20px]">Contact Us</h1>
+        <div className="flex flex-col sm:flex-row sm:gap-4 ">
           <div>
-            <p>First Name*</p>
+            <p className="pb-[10px] pt-[10px] text-[17px] ">First Name *</p>
             <input
-              className="bg-gray-200 rounded-[5px] h-[30px] px-[16px]"
+              className="bg-gray-100 border border-gray-400 rounded-[8px] h-[60px] w-[100%] px-[16px] lg:h-[50px] lg:w-[320px]"
               type="text"
             ></input>
           </div>
           <div>
-            <p>Last Name*</p>
+            <p className=" pb-[10px] pt-[10px] text-[17px] ">Last Name *</p>
             <input
-              className="bg-gray-200 rounded-[5px] h-[30px] px-[16px]"
+              className="bg-gray-100 border border-gray-400 rounded-[8px] h-[60px] w-[100%] px-[16px]  lg:h-[50px] lg:w-[320px]"
               type="text"
             ></input>
           </div>
         </div>
-        <div className="grid m-[12px]">
-          <p>Email Address*</p>
+        <div className="grid">
+          <p className=" pb-[10px] pt-[10px] text-[17px] ">Email Address *</p>
           <input
-            className="bg-gray-200 rounded-[5px] h-[30px]"
+            className="bg-gray-100 border border-gray-400 rounded-[8px] h-[60px] w-[100%] lg:h-[50px] lg:w-[100%]" 
             type="email"
           ></input>
         </div>
 
-        <div className="m-[12px]">
-          <p className="mb-2">Query Type*</p>
-          <div className="flex">
-            <div className="bg-gray-200 rounded-[5px] h-[30px] flex items-center w-[265px]">
-              <input className="m-3" type="radio" name="base" />
+        <div>
+          <p className=" pb-[10px] pt-[10px] text-[17px] ">Query Type *</p>
+          <div className="flex flex-col gap-3">
+            <div className="bg-gray-100 border border-gray-400 rounded-[8px] h-[60px] flex items-center w-[100%] lg:h-[50px] lg:w-[100%]">
+              <input className="m-5" type="radio" name="base" />
               <p>General Enquiry</p>
             </div>
-            <div className="bg-gray-200 rounded-[5px] h-[30px] ml-[10px] flex items-center w-[265px] ">
-              <input className="m-3" type="radio" name="base" />
+            <div className="bg-gray-100 border border-gray-400 rounded-[8px] h-[60px] flex items-center w-[100%]  lg:h-[50px] lg:w-[100%]">
+              <input className="m-5" type="radio" name="base" />
               <p>Support Request</p>
             </div>
           </div>
         </div>
 
-        <div className="m-[12px]">
-          <p>Message*</p>
-          <textarea className="bg-gray-200 resize-none rounded-[5px] h-[70px] w-[515px] p-1 pl-2"></textarea>
+        <div>
+          <p className=" pb-[10px] pt-[10px] text-[17px] ">Message *</p>
+          <textarea className="bg-gray-100 border border-gray-400 resize-none rounded-[8px] h-[200px] w-[100%] p-1 pl-2  lg:h-[100px] lg:w-[100%]"></textarea>
         </div>
 
         <div className="flex gap-4 items-center m-[12px]">
-          <input className="bg-gray-200 rounded-[5px] h-[30px]" type="checkbox" />
-          I consent to being contacted by the team*
+          <input className="bg-gray-100 border border-gray-400 rounded-[8px] h-[30px]" type="checkbox" />
+          I consent to being contacted by the team *
         </div>
-          <button className="bg-[rgba(12,125,105,255)] rounded-[5px] h-[40px] m-[12px] text-white hover:bg-[rgba(10,100,90,250)] duration-[0.3s] hover:scale-[1.03]">Submit</button>
+          <button className="bg-[rgba(12,125,105,255)] rounded-[8px] h-[40px] m-[12px] text-white hover:bg-[rgba(10,100,90,250)] duration-[0.3s] hover:scale-[1.03]">Submit</button>
       </div>
     </main>
   );
